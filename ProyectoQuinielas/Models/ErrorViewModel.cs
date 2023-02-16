feebf0c668b407d924b90b351f1aa2eb25a72388
FranciscoJMorales <55027785+FranciscoJMorales@users.noreply.github.com>
2023-02-16 10:57:19 -0600

    Agregar archivos de proyecto.

---
+++ ProyectoQuinielas/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProyectoQuinielas.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
