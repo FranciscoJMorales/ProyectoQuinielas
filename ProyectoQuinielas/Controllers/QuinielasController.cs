92ec3c6c624db3432d625d070d021f1a40ce7a87
FranciscoJMorales <55027785+FranciscoJMorales@users.noreply.github.com>
2023-02-22 18:51:47 -0600

    Updated Layout

---
+++ ProyectoQuinielas/Controllers/QuinielasController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoQuinielas.Controllers
+{
+    public class QuinielasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
