5886ad27d4a04e99eaff066eaea48501a6456e96
FranciscoJMorales <55027785+FranciscoJMorales@users.noreply.github.com>
2023-04-14 20:53:16 -0600

    Updated pool views

--- QuinielasModel/DTO/Pools/UpdatePool.cs
+++ QuinielasModel/DTO/Pools/UpdatePool.cs
@@ -1,8 +1,14 @@
-﻿namespace QuinielasModel.DTO.Pools;
+﻿using System.ComponentModel;
+
+namespace QuinielasModel.DTO.Pools;
 
 public class UpdatePool
 {
     public int Id { get; set; }
+
+    [DisplayName("Nombre de la quiniela")]
     public string Name { get; set; } = null!;
+
+    [DisplayName("Límite de usuarios")]
     public int UsersLimit { get; set; }
 }
