a7b939b541a0a8691dd10278771afc90eb0f2b4f
FranciscoJMorales <55027785+FranciscoJMorales@users.noreply.github.com>
2023-04-14 19:13:16 -0600

    Created new views

--- QuinielasModel/DTO/Users/PoolUsers.cs
+++ QuinielasModel/DTO/Users/PoolUsers.cs
@@ -1,12 +1,12 @@
-﻿namespace QuinielasModel.DTO.Games;
+﻿namespace QuinielasModel.DTO.Users;
 
-public class PoolGames : Result
+public class PoolUsers
 {
     public int PoolId { get; set; }
 
     public string PoolName { get; set; } = null!;
 
-    public IEnumerable<Game>? Games { get; set; }
+    public IEnumerable<UserId>? Users { get; set; }
 
     public int AdminId { get; set; }
 }
