e4354492e2a64c63cb6a3362e372f8676cbc7de4
FranciscoJMorales <55027785+FranciscoJMorales@users.noreply.github.com>
2023-04-17 12:49:17 -0600

    Added view for pool users

--- QuinielasWeb/Controllers/UsersController.cs
+++ QuinielasWeb/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using QuinielasWeb.Services;
 using QuinielasModel.DTO.Users;
+using QuinielasModel.DTO.Pools;
 
 namespace QuinielasWeb.Controllers
 {
@@ -8,11 +9,13 @@ namespace QuinielasWeb.Controllers
     {
         private readonly ILogger<UsersController> _logger;
         private readonly UsersService _usersService;
+        private readonly PoolsService _poolsService;
 
-        public UsersController(ILogger<UsersController> logger, UsersService usersService)
+        public UsersController(ILogger<UsersController> logger, UsersService usersService, PoolsService poolsService)
         {
             _logger = logger;
             _usersService = usersService;
+            _poolsService = poolsService;
         }
 
         [HttpGet]
@@ -103,6 +106,52 @@ namespace QuinielasWeb.Controllers
                 return RedirectToAction("login", "Home");
             var result = await _usersService.DeleteUser(id);
             HttpContext.Session.Clear();
+            return new JsonResult(result.Alert);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Pool(int id)
+        {
+            var userid = HttpContext.Session.GetInt32("userid");
+            if (userid == null)
+                return RedirectToAction("login", "Home");
+            ViewBag.User = HttpContext.Session.GetString("username");
+            var poolGames = await _poolsService.GetPoolUsers(id);
+            if (poolGames == null)
+                return NotFound();
+            if (poolGames.AdminId != userid)
+                return Unauthorized();
+            return View(poolGames);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Invite(InviteUser invitation)
+        {
+            var userid = HttpContext.Session.GetInt32("userid");
+            if (userid == null)
+                return RedirectToAction("login", "Home");
+            var result = await _poolsService.Invite(invitation);
+            ViewBag.User = HttpContext.Session.GetString("username");
+            ViewBag.Alert = result.Alert!.Alert;
+            ViewBag.AlertIcon = result.Alert.AlertIcon;
+            ViewBag.AlertMessage = result.Alert.AlertMessage;
+            ViewBag.RedirectUrl = result.Alert.RedirectUrl;
+            var poolGames = await _poolsService.GetPoolUsers(invitation.PoolId);
+            if (poolGames == null)
+                return NotFound();
+            if (poolGames.AdminId != userid)
+                return Unauthorized();
+            return View("Pool", poolGames);
+        }
+
+        [HttpPost]
+        [Route("/users/remove/{poolid}/{id}")]
+        public async Task<IActionResult> Remove(int poolid, int id)
+        {
+            var userid = HttpContext.Session.GetInt32("userid");
+            if (userid == null)
+                return RedirectToAction("login", "Home");
+            var result = await _poolsService.Remove(poolid, id);
             return new JsonResult(result.Alert);
         }
     }
